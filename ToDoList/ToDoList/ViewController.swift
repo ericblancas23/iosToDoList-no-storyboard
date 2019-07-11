5a27c53315ed22fcc99e43f637ee60d2056ad9ad
Eric Blancas <ericblancas23@gmail.com>
2019-07-11 15:42:33 -0700

    setting up view collections and header update func

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -22,27 +22,28 @@ class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLa
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
     }
+    //ordering task cells
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let taskCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! TaskCell
         taskCell.nameLabel.text = "Sample Task \(indexPath.item)"
         return taskCell
     }
-    
+    //sizing of the collection view
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 50)
+        return CGSize(width: view.frame.width, height: 60)
     }
-    
+    //
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath)
     }
-    
+    //dynamic sizing
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
 //        collectionVie
 //        return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "headerId", for: indexPath)
         return CGSize(width: view.frame.width, height: 100)
     }
 }
-
+//header component
 class TaskHeader: UICollectionViewCell {
     
     override init(frame: CGRect) {
@@ -73,7 +74,7 @@ class TaskHeader: UICollectionViewCell {
 }
 
 
-
+//single task cell
 class TaskCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
